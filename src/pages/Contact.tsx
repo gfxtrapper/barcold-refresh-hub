2591c38d74919eccbab0d80aa29031ba5a055f34
Trapper GFX <gfxtrapper@gmail.com>
2026-03-01 18:17:07 +0300

    Update Google Maps embed source in Contact page

--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -56,7 +56,7 @@ const Contact = () => {
             <div className="overflow-hidden rounded-xl border">
               <iframe
                 title="Barcold Location"
-                src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3988.819!2d36.849!3d-1.303!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x0!2zMcKwMTgnMTAuOCJTIDM2wrA1MCc1Ni40IkU!5e0!3m2!1sen!2ske!4v1610000000000!5m2!1sen!2ske"
+                src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3988.843553312673!2d36.979322474045546!3d1.2665441356029272!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x182f6b4edd48cbff%3A0x1dc86afd9ee2a40e!2sHybrid%20Kenya!5e0!3m2!1sen!2ske!4v1772377769576!5m2!1sen!2ske"
                 width="100%"
                 height="200"
                 style={{ border: 0 }}
