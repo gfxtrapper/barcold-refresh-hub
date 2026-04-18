0ee4d38765dc13919d16f638d313a50e5e61ec2d
Trapper GFX <gfxtrapper@gmail.com>
2026-04-18 09:56:22 -0700

    Updated quote constants for RequestQuote page

---
+++ src/constants/quote.ts
@@ -0,0 +1,31 @@
+// src/constants/quote.ts
+
+export const serviceOptions = [
+    'service1',
+    'service2',
+    'service3',
+];
+
+export const propertyTypes = [
+    'type1',
+    'type2',
+    'type3',
+];
+
+export const budgetRanges = [
+    { min: 0, max: 10000 },
+    { min: 10001, max: 50000 },
+    { min: 50001, max: 100000 },
+];
+
+export const timelines = [
+    'short-term',
+    'mid-term',
+    'long-term',
+];
+
+export const referralSources = [
+    'source1',
+    'source2',
+    'source3',
+];
